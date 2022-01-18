9ee6b1134b721ec5c1d09c03645db1ddae28789e
tamagoyakii <rlawlgus4063@naver.com>
2022-01-18 17:01:40 +0900

    [Feat] Selector 만들기 Close #6

--- client/src/components/Selector/styled.ts
+++ client/src/components/Selector/styled.ts
@@ -1,1 +1,44 @@
 import styled from 'styled-components';
+
+export const SelContainer = styled.div`
+	height: 14rem;
+	width: 25rem;
+	border: solid 2px black;
+`;
+
+export const Topbar = styled.div`
+	height: 1.8rem;
+	background: #E0E0E0;
+`;
+
+export const MainContainer = styled.div`
+	height: 12.2rem;
+	padding: 2rem;
+	display: flex;
+	flex-direction: column;
+	justify-content: space-evenly;
+	align-items: center;
+`;
+
+export const Phrases = styled.div`
+`;
+
+export const Buttons = styled.div`
+	display: flex;
+	justify-content: space-evenly;
+	input{
+		height: 2.5rem;
+		width: 6rem;
+		margin: 0.3rem;
+		display: flex;
+		align-items: center;
+		justify-content: center;
+	}
+	.positive{
+		background: black;
+		color: white;
+	}
+	.negative{
+		background: #E0E0E0;
+	}
+`;
