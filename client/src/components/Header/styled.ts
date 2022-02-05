2589932b3fba7c208c35ed5d8b4da2bd6fe46d77
tamagoyakii <rlawlgus4063@naver.com>
2022-02-05 17:01:45 +0900

    [Feat] Header 프로필 클릭 시 드롭다운 메뉴 생성 Close #4

--- client/src/components/Header/styled.ts
+++ client/src/components/Header/styled.ts
@@ -8,6 +8,10 @@ export const Container = styled.div`
   padding: 0.6vh 3vw 0.6vh 0.6vw;
 `;
 
+export const UserProfile = styled.div`
+	display: flex;
+`;
+
 export const UserImg = styled.div`
   display: flex;
   align-items: center;
@@ -22,4 +26,22 @@ export const UserName = styled.div`
   align-items: center;
   margin: 1.5vh;
   font-size: 2vh;
+`;
+
+export const DropdownMenu = styled.div`
+	margin: 6vh 3vw 0 0;
+	padding: 0.4rem 1rem 0.4rem 1rem;
+	border: solid 1px black;
+	position: absolute;
+	top: 0;
+  right: 0;
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+	justify-content: space-evenly;
+	li {
+		padding: 0.2rem;
+		list-style: none;
+		font-size: 1.5vh;
+	}
 `;
