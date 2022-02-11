6d1dc289f15900daf4d883e3f158a30c7d86ce37
pearpearB <smilyoung@gmail.com>
2022-02-11 13:39:27 +0900

    [Fix] CSS rem으로 수정, top decoration 추가

--- client/src/pages/Login/Login.tsx
+++ client/src/pages/Login/Login.tsx
@@ -8,7 +8,16 @@ function Login() {
 		<AppContainer>
 			<LoginContainer>
 				<TopWrap>
-					<div className='xIcon'>&times;</div>
+						<div className='decoration'>
+							<div className='visibleDot'></div>
+							<div className='visibleDot'></div>
+							<div className='visibleDot'></div>
+						</div>
+						<div className='decoration'>
+							<div></div>
+							<div></div>
+							<div></div>
+						</div>
 				</TopWrap>
 				<SeoulImg>
 					<img src="/images/logo42.png" alt='42img' />
@@ -16,12 +25,12 @@ function Login() {
 				<TextWrap>
 					<div className='BiggerText'>Born to Code?</div>
 					<div className='SmallerText'>
-					42seoul 카뎃만 참여할 수 있습니다.<br/>
-					계정이 존재하는 지 확인해주세요.
+						42seoul 카뎃만 참여할 수 있습니다.<br/>
+						계정이 존재하는 지 확인해주세요.
 					</div>
 				</TextWrap>
 				<ButtonWrap>
-					<input type="button" value="로그인" />
+					<button>로그인</button>
 				</ButtonWrap>
 			</LoginContainer>
 		</AppContainer>
