1df8edf369884b04b2d5ffdce761f79ae8f620f4
tamagoyakii <rlawlgus4063@naver.com>
2022-02-13 14:06:30 +0900

    [Fix] CSS 수정

--- client/src/pages/Login/Login.tsx
+++ client/src/pages/Login/Login.tsx
@@ -1,39 +1,41 @@
-import { AppContainer, LoginContainer, TopWrap, SeoulImg
-				, TextWrap, ButtonWrap } from './styled';
+import { Link } from 'react-router-dom';
+import { PageName, Squares } from 'styles/styled'
+import { AppContainer, PageContainer, TopBar, ContentContainer
+				, SeoulImg, TextWrap, ButtonWrap } from './styled'
 
 function Login() {
 
 	return (
 		<>
-		<AppContainer>
-			<LoginContainer>
-				<TopWrap>
-						<div className='decoration'>
-							<div className='visibleDot'></div>
-							<div className='visibleDot'></div>
-							<div className='visibleDot'></div>
-						</div>
-						<div className='decoration'>
-							<div></div>
-							<div></div>
-							<div></div>
-						</div>
-				</TopWrap>
-				<SeoulImg>
-					<img src="/images/logo42.png" alt='42img' />
-				</SeoulImg>
-				<TextWrap>
-					<div className='BiggerText'>Born to Code?</div>
-					<div className='SmallerText'>
-						42seoul 카뎃만 참여할 수 있습니다.<br/>
-						계정이 존재하는 지 확인해주세요.
-					</div>
-				</TextWrap>
-				<ButtonWrap>
-					<button>로그인</button>
-				</ButtonWrap>
-			</LoginContainer>
-		</AppContainer>
+			<AppContainer>
+				<PageContainer>
+					<TopBar>
+						<PageName>login</PageName>
+						<Squares>
+							<div>&#9866;</div>
+							<div>&#10064;</div>
+							<Link to='/'>
+								<div>&times;</div>
+							</Link>
+						</Squares>
+					</TopBar>
+					<ContentContainer>
+						<SeoulImg>
+							<img src="/images/logo42.png" alt='42img' />
+						</SeoulImg>
+						<TextWrap>
+							<div>Born to Code?</div>
+							<div>
+								42seoul 카뎃만 참여할 수 있습니다.<br/>
+								계정이 존재하는 지 확인해주세요.
+							</div>
+						</TextWrap>
+						<ButtonWrap>
+							<button>로그인</button>
+						</ButtonWrap>
+					</ContentContainer>
+				</PageContainer>
+			</AppContainer>
 		</>
 	);
 }
