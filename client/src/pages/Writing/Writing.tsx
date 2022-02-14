7e5efde732382aaf8021a8a6ebe385e97790e4fe
tamagoyakii <rlawlgus4063@naver.com>
2022-02-14 19:08:31 +0900

    [Feat] ToastEditior 설치 Close #59

--- client/src/pages/Writing/Writing.tsx
+++ client/src/pages/Writing/Writing.tsx
@@ -1,5 +1,26 @@
+import Header from 'components/Header/Header';
+import '@toast-ui/editor/dist/toastui-editor.css';
+import { Editor } from '@toast-ui/react-editor';
+import { AppContainer, PageContainer } from 'styles/styled';
+
 function Writing() {
-  return <></>;
+
+  return (
+	<>
+		<AppContainer>
+			<Header />
+			<PageContainer>
+				<Editor
+					initialValue="hello react editor world!"
+					previewStyle="vertical"
+				  height="600px"
+				  initialEditType="markdown"
+				  useCommandShortcut={true} />
+			<div>test</div>
+			</PageContainer>
+		</AppContainer>
+	</>
+	);
 }
 
 export default Writing;
