430f08f869bb856977b409254e43ff4ad6356182
tamagoyakii <rlawlgus4063@naver.com>
2022-02-17 23:53:44 +0900

    [Delete] 필요 없는 요소  정리 Close #100

--- client/src/pages/Blindboard/Blindboard.tsx
+++ client/src/pages/Blindboard/Blindboard.tsx
@@ -2,7 +2,7 @@ import { useState, useEffect, useRef } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import Header from 'components/Header/Header';
 import Footer from 'components/Footer/Footer';
-import Postpreview from 'components/Postpreview/Postpreview';
+import PostPreview from 'components/Postpreview/Postpreview';
 import PageNation from 'components/PageNation/PageNation';
 import instance from 'utils/functions/axios';
 import { AppContainer, PageContainer, TopBar, PageName, Squares, Category } from 'styles/styled';
@@ -10,30 +10,10 @@ import { PostContainer, PostWrap, ContentWrap } from './styled';
 import { BoardData, ContentData} from 'utils/functions/type';
 
 function Blindboard() {
-	const [boardData, setBoardData] = useState<BoardData>({
-		contents: [
-			{	id: 0,
-				authorId: 0,
-				title: '',
-				commentCnt: 0,
-				viewCnt: 0,
-				likeCnt: 0,
-				isNotice: false,
-				blameCnt: 0,
-				createdDate: '',
-				modifiedDate: ''
-			}
-		], 
-		page: 0, 
-		pages: 0
-	});
-	const [curPage, setCurPage] = useState<number>(0);
-	// const [isLoading, setIsLoading] = useState(true);
-	// const [currentMenu, setCurrentMenu] = useState(-1);
+	const [boardData, setBoardData] = useState({contents: [], page: 0, pages: 0});
+	const {page, pages} = boardData
 	const currentUrl = window.location.href;
 	const urlId = currentUrl.split('blindboard?page=')[1];
-	
-	const {page, pages} = boardData
 
 	useEffect(() => {
 		instance
@@ -42,12 +22,8 @@ function Blindboard() {
 		.catch((err) => console.log(err));
 	}, [])
 
-	// const postData = boardData.contents;
-	// const pageData = {page: boardData.page, pages: boardData.pages};
-
 	const pageChangeHandler = (page: number) => {
 		window.location.href = `/blindboard?page=${page}`
-		// setCurPage(page)
   };
 
 	return (
@@ -75,7 +51,7 @@ function Blindboard() {
 						<ContentWrap>
 							<PostWrap>
 								{boardData.contents.map((el: ContentData, idx) => {
-									return (<Postpreview key={idx} content={el} />)
+									return (<PostPreview key={idx} postData={el} />)
 								})}
 							</PostWrap>
 						</ContentWrap>
