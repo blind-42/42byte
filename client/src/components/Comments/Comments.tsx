77dde71319d5bad65115d1fc3919b6d9aa5f921d
pearpearB <smilyoung@gmail.com>
2022-02-20 00:16:43 +0900

    [Feat] 댓글 수정 구현 Close #117

--- client/src/components/Comments/Comments.tsx
+++ client/src/components/Comments/Comments.tsx
@@ -2,7 +2,8 @@ import { useState } from 'react';
 import instance from 'utils/functions/axios';
 import DeleteModal from 'components/Modal/DeleteModal';
 import { CommentData } from 'utils/functions/type';
-import { CommentWrap, CommentTop, Info, Modify, Content, Line } from './styled'
+import { CommentWrap, ModifyCommentWrap, CommentTop, Info, Modify, Content, GLine, FLine } from './styled'
+import { CommentInput } from 'pages/Detail/styled'
 
 type GreetingProps = {
 	comment: CommentData
@@ -12,14 +13,31 @@ type GreetingProps = {
 function Comments({comment, commentsUserList}: GreetingProps) {
 	const { boardId, postId, id, authorId, content, likeCnt, blameCnt, isUsers, isAuthor, isDel, createdDate, modifiedDate } = comment;
 	const [openCmmtDelModal, setOpenCmmtDelModal] = useState<boolean>(false);
+	const [modifyState, setModifyState] = useState<boolean>(false);
+	const [modifyCmmt, setModifyCmmt] = useState<string>(content || '');
 
 	const clickCmmtDelModalHandler = () => {
 		setOpenCmmtDelModal(!openCmmtDelModal);
 	}
 
+	const modifyHandler = () => {
+		setModifyState(!modifyState);
+	}
+
+	const inputModifyCmmtHandler = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+		setModifyCmmt(e.target.value);
+	}
+	
 	const deleteCmmtHandler = () => {
 		instance
 		.delete(`/comment?commentId=${id}`)
+		.then(() => {window.location.href = `/detail?boardId=${boardId}&postId=${postId}`})
+		.catch((err) => console.log(err));
+	}
+
+	const sendModifyCmmtHandler = () => {
+		instance
+		.put(`/comment?commentId=${id}`)
 		.then(() => {window.location.href = `/detail?boardId=${boardId}&postId=${postId}`})
 		.catch((err) => console.log(err));
 	}
@@ -29,7 +47,29 @@ function Comments({comment, commentsUserList}: GreetingProps) {
 		{openCmmtDelModal && (
 			<DeleteModal clickModalHandler={clickCmmtDelModalHandler}
 										deleteHandler={deleteCmmtHandler}/>)}
-			<CommentWrap>
+			{modifyState
+			? <ModifyCommentWrap>
+					<CommentTop>
+						<Info>
+							{isAuthor ? <h3>작성자</h3>
+								: isUsers ? <h3><u>카뎃 {commentsUserList.indexOf(authorId)+1}</u></h3>
+								: <h3>카뎃 {commentsUserList.indexOf(authorId)+1}</h3>}
+						</Info>
+						<Modify>
+							<div onClick={modifyHandler}>취소</div>
+						</Modify>
+					</CommentTop>
+					<Content>
+						<CommentInput>
+							<textarea defaultValue={content} onChange={inputModifyCmmtHandler} maxLength={300} />
+							<div>
+								<span>{modifyCmmt.length} / 300</span>
+								<input type='button' value='등록' onClick={sendModifyCmmtHandler}/>
+							</div>
+						</CommentInput>
+					</Content>
+				</ModifyCommentWrap>
+			: <CommentWrap>
 				<CommentTop>
 					<Info>
 						{isAuthor ? <h3>작성자</h3>
@@ -38,7 +78,7 @@ function Comments({comment, commentsUserList}: GreetingProps) {
 						<div>{createdDate?.slice(5, 10)}</div>
 					</Info>
 					{(isUsers && !isDel) && <Modify>
-						<div>수정</div>
+						<div onClick={modifyHandler}>수정</div>
 						<div onClick={clickCmmtDelModalHandler}>삭제</div>
 					</Modify>}
 				</CommentTop>
@@ -47,7 +87,9 @@ function Comments({comment, commentsUserList}: GreetingProps) {
 								: <div>{content}</div>}
 				</Content>
 			</CommentWrap>
-			<Line/>
+			}
+			<GLine/>
+			<FLine/>
 		</>
 	);
 }
