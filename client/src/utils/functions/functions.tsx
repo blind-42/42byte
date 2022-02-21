f2f5606e1b081387b640439299bb57780ad4f144
tamagoyakii <rlawlgus4063@naver.com>
2022-02-21 18:00:41 +0900

    [Fix] timeForToday 로직 변경 Close #158

--- client/src/utils/functions/functions.tsx
+++ client/src/utils/functions/functions.tsx
@@ -9,23 +9,26 @@ export const shortening = (param: string, length: number) => {
 export const timeForToday = (value: string) => {
 	const timeValue = new Date(value);
 	const today = new Date();
-	// const year = `${fullday.getFullYear()}`;
-	// const month = fullday.getMonth() + 1 < 10 ? `0${fullday.getMonth() + 1}` : `${fullday.getMonth() + 1}`;
-	// const date = `${fullday.getDate()}`;
-	// const today = year + '-' + month + '-' + date;
+	const year = `${today.getFullYear()}`;
+	const month = today.getMonth() + 1 < 10 ? `0${today.getMonth() + 1}` : `${today.getMonth() + 1}`;
+	const date = `${today.getDate()}`;
+	const fullday = year + '-' + month + '-' + date;
 
 	const betweenTime = Math.floor((today.getTime() - timeValue.getTime()) / 1000 / 60);
 	if (betweenTime < 1) return '방금전';
 	if (betweenTime < 60) {
-			return `${betweenTime} 분전`;
+			return `${betweenTime}분전`;
 	}
 
-	const betweenTimeHour = Math.floor(betweenTime / 60);
-	if (betweenTimeHour < 24) {
-			return value.slice(11, 16);
-	}
+	if (fullday.slice(0, 10) === value.slice(0,10))
+		return value.slice(11, 16);
 
 	return value.slice(2,10);
+
+	// const betweenTimeHour = Math.floor(betweenTime / 60);
+	// if (betweenTimeHour < 24) {
+	// 		return `${betweenTimeHour}시간전`;
+	// }
 
 	// const betweenTimeDay = Math.floor(betweenTime / 60 / 24);
 	// if (betweenTimeDay < 365) {
