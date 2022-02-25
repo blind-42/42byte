8696704b78adac1c313a581adfb3fa46311f6985
tamagoyakii <rlawlgus4063@naver.com>
2022-02-25 13:08:01 +0900

    [Feat] Utils(글쓰기 버튼, 검색창) 추가 Close #182

--- client/src/pages/Blindboard/Blindboard.tsx
+++ client/src/pages/Blindboard/Blindboard.tsx
@@ -6,13 +6,13 @@ import Footer from 'components/Footer/Footer';
 import PostPreview from 'components/Postpreview/Postpreview';
 import PageNation from 'components/PageNation/PageNation';
 import instance from 'utils/functions/axios';
-import { AppContainer, PageContainer, TopBar, PageName, Squares, ContentFooterWrap, Category } from 'styles/styled';
-import { PostContainer, PostWrap, ContentWrap } from './styled';
+import { AppContainer, PageContainer, TopBar, PageName, Squares, PostContainer, ContentFooterWrap, Category } from 'styles/styled';
+import { UtilPostWrap, UtilWrap, Search, WritingButton, PostWrap, ContentWrap } from './styled';
 import { BoardData, ContentData} from 'utils/functions/type';
 import Loading from 'pages/Loading/Loading';
 import Error from 'pages/Error/Error';
 
-function Blindboard() {
+export default function Blindboard() {
 	const [boardData, setBoardData] = useState({contents: [], page: 0, pages: 0});
 	const {page, pages} = boardData
 	const currentUrl = window.location.href;
@@ -54,27 +54,42 @@ function Blindboard() {
 						</Squares>
 					</TopBar>
 					<ContentFooterWrap>
-						<PostContainer>
-							<Category>
-								<div></div>
-								<div>제목</div>
-								<div>조회</div>
-								<div>추천</div>
-								<div>작성일</div>
-							</Category>
-							<ContentWrap>
-								<PostWrap>
-									{boardData.contents.map((el: ContentData, idx) => {
-										return (<PostPreview key={idx} postData={el} />)
-									})}
-								</PostWrap>
-							</ContentWrap>
-							<PageNation
-								curPage={page}
-								totalPages={pages}
-								pageChangeHandler={pageChangeHandler}
-							/>
-						</PostContainer>
+						<UtilPostWrap>
+							<UtilWrap>
+								<Search>
+									<input type="text" placeholder='검색어를 입력하세요'/>
+									<button>
+										<div>&#9740;</div>
+									</button>
+								</Search>
+								<WritingButton>
+									<Link to="/writing">
+										<input type="button" value="글쓰기" />
+									</Link>
+								</WritingButton>
+							</UtilWrap>
+							<PostContainer>
+								<Category>
+									<div></div>
+									<div>제목</div>
+									<div>조회</div>
+									<div>추천</div>
+									<div>작성일</div>
+								</Category>
+								<ContentWrap>
+									<PostWrap>
+										{boardData.contents.map((el: ContentData, idx) => {
+											return (<PostPreview key={idx} postData={el} />)
+										})}
+									</PostWrap>
+								</ContentWrap>
+								<PageNation
+									curPage={page}
+									totalPages={pages}
+									pageChangeHandler={pageChangeHandler}
+								/>
+							</PostContainer>
+						</UtilPostWrap>
 						<Footer />
 					</ContentFooterWrap>
 				</PageContainer>
@@ -82,5 +97,3 @@ function Blindboard() {
 		</>
 	);
 }
-
-export default Blindboard;
--- client/src/pages/Blindboard/styled.ts
+++ client/src/pages/Blindboard/styled.ts
@@ -1,13 +1,120 @@
 import styled from 'styled-components';
 
-export const PostContainer = styled.div`
-	overflow-y: scroll;
+export const UtilPostWrap = styled.div`
+	display: grid;
+	grid-template-rows: 2.5rem auto;
+	${({theme}) => theme.media.tablet`
+		grid-template-rows: 1.8rem auto;
+	`}
+	${({theme}) => theme.media.desktop`
+		grid-template-rows: 1.8rem auto;
+	`}
+`;
+
+export const UtilWrap = styled.div`
 	background: #C4C4C4;
+	display: flex;
+	align-items: center;
+	justify-content: end;
+`;
+
+export const Search = styled.div`
+	display: flex;
+	justify-content: space-between;
+	align-items: center;
 	border-top: solid 2px #000;
 	border-left: solid 2px #000;
 	border-right: solid 2px #fff;
 	border-bottom: solid 2px #fff;
-	padding: 3px;
+	height: 2.3rem;
+	background: #fff;
+	input {
+		font-size: 1.1rem;
+		display: flex;
+		padding-left: 0.2rem;
+		border: none;
+		width: 15rem;
+	}
+	button {
+		cursor: pointer;
+		margin-right: -2px;
+		background: #518EDB;
+		border-top: solid 2px #fff;
+		border-left: solid 2px #fff;
+		border-right: solid 2px #000;
+		border-bottom: solid 2px #000;
+		color: #fff;
+		height: 2.3rem;
+		width: 2.3rem;
+		div {
+			font-size: 1.2rem;
+			transform: rotate(100deg);
+		}
+	}
+	${({theme}) => theme.media.tablet`
+		height: 1.6rem;
+		input {
+			font-size: 0.8rem;
+			width: 12rem;
+		}
+		button {
+			height: 1.6rem;
+			width: 1.6rem;
+			div {
+				font-size: 1rem; 
+			}
+		}
+	`}
+	${({theme}) => theme.media.desktop`
+		height: 1.6rem;
+		input {
+			font-size: 0.8rem;
+			width: 12rem;
+		}
+		button {
+			height: 1.6rem;
+			width: 1.6rem;
+			div {
+				font-size: 1rem; 
+			}
+		}
+	`}
+`;
+
+export const WritingButton = styled.div`
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+	margin-left: 3px;
+	input {
+		cursor: pointer;
+		background: #518EDB;
+		border-top: solid 2px #fff;
+		border-left: solid 2px #fff;
+		border-right: solid 2px #000;
+		border-bottom: solid 2px #000;
+		font-size: 1.2rem;
+		font-weight: 600;
+		height: 2.3rem;
+		width: 6.5rem;
+		color: #fff;
+	}
+	${({theme}) => theme.media.tablet`
+		input {
+			font-size: 0.9rem;
+			font-weight: 700;
+			height: 1.6rem;
+			width: 5rem;
+		}
+	`}
+	${({theme}) => theme.media.desktop`
+		input {
+			font-size: 0.9rem;
+			font-weight: 700;
+			height: 1.6rem;
+			width: 5rem;
+		}
+	`}
 `;
 
 export const ContentWrap = styled.div`
