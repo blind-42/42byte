be6b9ba7712177ccac2365b86a76d381826f0e7a
tamagoyakii <rlawlgus4063@naver.com>
2022-02-26 22:18:31 +0900

    [Fix] 삭제 모달핸들러 위치 변경 및 변수명 수정 Close #206

--- client/src/components/DropdownMenu/DropdownMenu.tsx
+++ client/src/components/DropdownMenu/DropdownMenu.tsx
@@ -1,53 +1,58 @@
 
 import React, { useState } from 'react';
 import ReportModal from 'components/Modal/ReportModal';
+import DeleteModal from 'components/Modal/DeleteModal';
 import { DropdownWrap, Dots, DropdownBox, MenuList } from './styled';
 
 type GreetingProps = {
 	isUsers: boolean
-	delModalHandler: () => void;
 	modifyHandler: () => void;
-	type: string
-	postId: number
-	commentId: number
+	deleteHandler: () => void;
+	reportHandler: (reportIssue: string) => void;
 }
 
-export default function DropdownMenu ({ isUsers, delModalHandler, modifyHandler, type, postId, commentId }: GreetingProps) {
+export default function DropdownMenu ({ isUsers, modifyHandler, deleteHandler, reportHandler }: GreetingProps) {
 	const [openDropdown, setOpenDropdown] = useState<boolean>(false);
+	const [openDelModal, setOpenDelModal] = useState<boolean>(false);
 	const [openRptModal, setOpenRptModal] = useState<boolean>(false);
+
+	const dropdownHandler = () => {
+		setOpenDropdown(!openDropdown);
+	}
+
+	const deleteModalHandler = () => {
+		setOpenDropdown(false);
+		setOpenDelModal(!openDelModal);
+	}
 
 	const reportModalHandler = () => {
 		setOpenDropdown(false);
 		setOpenRptModal(!openRptModal);
 	}
-	
-	const dropdownHandler = () => {
-		setOpenDropdown(!openDropdown);
-	}
 
 	return (
 		<>
 			<DropdownWrap>
-			{openRptModal && (
-				<ReportModal clickModalHandler={reportModalHandler} type={type} postId={postId} commentId={commentId} />)}
+			{openDelModal &&
+			<DeleteModal clickModalHandler={deleteModalHandler} deleteHandler={deleteHandler} />}
+			{openRptModal &&
+			<ReportModal clickModalHandler={reportModalHandler} reportHandler={reportHandler} />}
 				<Dots onClick={dropdownHandler}>
 					<div></div>
 					<div></div>
 					<div></div>
 				</Dots>
-			{openDropdown &&
+				{openDropdown &&
 				<DropdownBox>
 					{isUsers
-						? <MenuList>
-								<div onClick={modifyHandler}>수정</div>
-								<div onClick={delModalHandler}>삭제</div>
-							</MenuList>
-						: <MenuList>
-								<div onClick={reportModalHandler}>신고</div>
-							</MenuList>
-					}
-				</DropdownBox>
-			}
+					? <MenuList>
+							<div onClick={modifyHandler}>수정</div>
+							<div onClick={deleteModalHandler}>삭제</div>
+						</MenuList>
+					: <MenuList>
+							<div onClick={reportModalHandler}>신고</div>
+						</MenuList>}
+				</DropdownBox>}
 			</DropdownWrap>
 		</>
 	);
