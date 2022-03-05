f8468f10c13c564679a92d4ebd3b6675c953225c
Jinhyun Park <developerjhp@gmail.com>
2022-03-06 03:51:57 +0900

    [Feat] 검색 기능 구현 close  #313

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,10 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { QueryClient, QueryClientProvider } from 'react-query'
+import { QueryClient, QueryClientProvider } from 'react-query';
 import { RecoilRoot } from 'recoil';
 import Main from './pages/Main/Main';
 import Login from './pages/Login/Login';
 import Board from './pages/Board/Board';
+import Search from './pages/Search/Search';
 import Writing from './pages/Writing/Writing';
 import Detail from './pages/Detail/Detail';
 import Mypage from './pages/Mypage/Mypage';
@@ -16,28 +17,28 @@ import { GlobalStyle } from './styles/global-style';
 const queryClient = new QueryClient();
 
 export default function App() {
-
   return (
     <>
-		<QueryClientProvider client={queryClient}>
-			<RecoilRoot>
-				<GlobalStyle />
-					<Router>
-						<Routes>
-							<Route path='/' element={<Main />} />
-							<Route path='/login' element={<Login />} />
-							<Route path='/board' element={<Board />} />
-							<Route path='/writing' element={<Writing />} />
-							<Route path='/detail' element={<Detail />} />
-							<Route path='/mypage' element={<Mypage />} />
-							<Route path='/notice' element={<Notice />} />
-							<Route path='/error' element={<Error />} />
-							<Route path='/loading' element={<Loading />} />
-							<Route path='/admin' element={<Admin />} />
-						</Routes>
-					</Router>
-				</RecoilRoot>
-			</QueryClientProvider>
+      <QueryClientProvider client={queryClient}>
+        <RecoilRoot>
+          <GlobalStyle />
+          <Router>
+            <Routes>
+              <Route path="/" element={<Main />} />
+              <Route path="/login" element={<Login />} />
+              <Route path="/board" element={<Board />} />
+              <Route path="/search" element={<Search />} />
+              <Route path="/writing" element={<Writing />} />
+              <Route path="/detail" element={<Detail />} />
+              <Route path="/mypage" element={<Mypage />} />
+              <Route path="/notice" element={<Notice />} />
+              <Route path="/error" element={<Error />} />
+              <Route path="/loading" element={<Loading />} />
+              <Route path="/admin" element={<Admin />} />
+            </Routes>
+          </Router>
+        </RecoilRoot>
+      </QueryClientProvider>
     </>
   );
 }
---
+++ client/src/pages/Search/styled.ts
@@ -0,0 +1,137 @@
+import styled from 'styled-components';
+
+export const UtilPostWrap = styled.div`
+	display: grid;
+	grid-template-rows: 2.5rem auto;
+	${({theme}) => theme.media.tablet`
+		grid-template-rows: 1.8rem auto;
+	`}
+	${({theme}) => theme.media.desktop`
+		grid-template-rows: 1.8rem auto;
+	`}
+`;
+
+export const UtilWrap = styled.div`
+	background: #C4C4C4;
+	display: flex;
+	align-items: center;
+	justify-content: end;
+`;
+
+export const Search = styled.div`
+	display: flex;
+	align-items: center;	
+	border-top: solid 2px #000;
+	border-left: solid 2px #000;
+	border-right: solid 2px #fff;
+	border-bottom: solid 2px #fff;
+	height: 2.3rem;
+	background: #fff;
+	form {
+		display: flex;
+		justify-content: space-between;
+		align-items: center;
+		input {
+			font-size: 1.1rem;
+			width: 15rem;
+			display: flex;
+			padding-left: 0.3rem;
+			border: none;
+		}
+		button {
+			cursor: pointer;
+			margin-right: -2px;
+			background: #518EDB;
+			border-top: solid 2px #fff;
+			border-left: solid 2px #fff;
+			border-right: solid 2px #000;
+			border-bottom: solid 2px #000;
+			color: #fff;
+			height: 2.3rem;
+			width: 2.3rem;
+			display: flex;
+			align-items: center;
+			justify-content: center;
+			div {
+				font-size: 1.2rem;
+			}
+		}
+	}
+	${({theme}) => theme.media.tablet`
+		height: 1.6rem;
+		form {
+			input {
+				font-size: 0.8rem;
+				width: 12rem;
+			}
+			button {
+				height: 1.6rem;
+				width: 1.6rem;
+				div {
+					font-size: 1rem; 
+				}
+			}
+		}
+	`}
+	${({theme}) => theme.media.desktop`
+		height: 1.6rem;
+		form {
+			input {
+				font-size: 0.8rem;
+				width: 12rem;
+			}
+			button {
+				height: 1.6rem;
+				width: 1.6rem;
+				div {
+					font-size: 0.8rem;
+				}
+			}
+		}
+	`}
+`;
+
+export const WritingButton = styled.div`
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+	margin-left: 3px;
+	input {
+		cursor: pointer;
+		background: #518EDB;
+		border-top: solid 2px #fff;
+		border-left: solid 2px #fff;
+		border-right: solid 2px #000;
+		border-bottom: solid 2px #000;
+		font-size: 1.2rem;
+		font-weight: 600;
+		height: 2.3rem;
+		width: 6.5rem;
+		color: #fff;
+	}
+	${({theme}) => theme.media.tablet`
+		input {
+			font-size: 0.9rem;
+			font-weight: 700;
+			height: 1.6rem;
+			width: 5rem;
+		}
+	`}
+	${({theme}) => theme.media.desktop`
+		input {
+			font-size: 0.9rem;
+			font-weight: 700;
+			height: 1.6rem;
+			width: 5rem;
+		}
+	`}
+`;
+
+export const ContentWrap = styled.div`
+	border-top: solid 1px #fff;
+	background: #D5D5D5;
+`;
+
+export const PostWrap = styled.div`
+	border-bottom: solid 2px #fff;
+`;
