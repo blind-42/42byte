6bc713c85166d3182bc0305f78d2540dd2a97432
tamagoyakii <rlawlgus4063@naver.com>
2022-03-14 16:59:46 +0900

    [Fix] pageContainer position:fixed 해제

--- client/src/styles/styled.ts
+++ client/src/styles/styled.ts
@@ -15,15 +15,11 @@ export const AppContainer = styled.div`
 `;
 
 export const PageContainer = styled.div`
-  height: 100vh;
   width: 100vw;
   background: #c4c4c4;
   padding: 3px;
   ${({ theme }) => theme.media.tablet`
-		position: fixed;
-		top: 50%;
-		left: 50%;
-		transform: translate(-50%, -53%);
+		margin: auto 0;
 		width: 90vw;
 		height: 80vh;
 		border-top: solid 2px #fff;
@@ -32,10 +28,7 @@ export const PageContainer = styled.div`
 		border-bottom: solid 2px #000;
 	`}
   ${({ theme }) => theme.media.desktop`
-		position: fixed;
-		top: 50%;
-		left: 50%;
-		transform: translate(-50%, -53%);
+		margin: auto 0;
 		width: 90vh;
 		height: 90vh;
 		border-top: solid 2px #fff;
