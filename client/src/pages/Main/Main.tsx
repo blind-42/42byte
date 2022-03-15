f64358b3c2ba5d5b84fb1facaa2bfe505ef05657
tamagoyakii <rlawlgus4063@naver.com>
2022-03-15 20:35:58 +0900

    [Feat] 메인페이지에 게시판으로 가는 아이콘 추가 Close #484

--- client/src/pages/Main/Main.tsx
+++ client/src/pages/Main/Main.tsx
@@ -1,16 +1,27 @@
 import { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { useRecoilState } from 'recoil';
+import { useQuery } from 'react-query';
+import { Link } from 'react-router-dom';
 import { LoginState } from 'States/LoginState';
-import { AiFillSetting } from 'react-icons/ai';
 import Header from '../../components/Header/Header';
+import LoginModal from 'components/Modal/LoginModal';
 import instance from 'utils/functions/axios';
+import { stringLimit } from 'utils/functions/functions';
 import { UserData } from 'utils/functions/type';
+import { BoardList, BoardPre } from 'utils/functions/type';
 import { AppContainer } from 'styles/styled';
-import { PageContainer, LogoImg, AdminPageButton } from './styled';
+import {
+  PageContainer,
+  LogoImg,
+  BoardIconWrap,
+  BoardIcon,
+  AdminPageButton,
+} from './styled';
 
 export default function Main() {
   const [isLoggedIn, setIsLoggedIn] = useRecoilState(LoginState);
+  const [openLoginModal, setOpenLoginModal] = useState<boolean>(false);
   const token = window.location.href.split('?token=')[1];
   const [userData, setUserData] = useState<UserData>({
     createdDate: '',
@@ -19,8 +30,24 @@ export default function Main() {
     profileImageUrl: '',
     roleType: '',
   });
-  const { createdDate, modifiedDate, hashId, profileImageUrl, roleType } =
-    userData;
+  const { roleType } = userData;
+  const [boardList, setBoardList] = useState<BoardList>({
+    contents: [],
+    page: 0,
+    pages: 0,
+  });
+  const { contents, page, pages } = boardList;
+  const { isFetching, isLoading, error, data } = useQuery(
+    ['menubar_key'],
+    () => {
+      instance.get('/board/list').then((res) => setBoardList(res.data));
+    },
+    {
+      retry: 0,
+      refetchOnWindowFocus: false,
+      keepPreviousData: true,
+    },
+  );
   const navigate = useNavigate();
 
   useEffect(() => {
@@ -45,6 +72,10 @@ export default function Main() {
       .catch((err) => console.log(err));
   }, []);
 
+  const openLoginModalHandler = () => {
+    setOpenLoginModal(!openLoginModal);
+  };
+
   const moveToAdmin = () => {
     instance
       .get('/admin')
@@ -56,19 +87,45 @@ export default function Main() {
   };
 
   return (
-    <AppContainer>
-      <PageContainer>
-        <LogoImg>
-          <img src="images/42byteLogo.png" />
-        </LogoImg>
-        {roleType === 'ADMIN' && (
-          <AdminPageButton onClick={moveToAdmin}>
-            <img src="images/adminIcon.png" alt="adminIcon" />
-            <div>Settings</div>
-          </AdminPageButton>
-        )}
-      </PageContainer>
-      <Header />
-    </AppContainer>
+    <>
+      {openLoginModal && (
+        <LoginModal openLoginModalHandler={openLoginModalHandler} />
+      )}
+      <AppContainer>
+        <PageContainer>
+          <LogoImg>
+            <img src="images/42byteLogo.png" />
+          </LogoImg>
+          <BoardIconWrap>
+            {contents.map((el: BoardPre, idx) => {
+              return (
+                !el.isDel &&
+                (isLoggedIn ? (
+                  <BoardIcon>
+                    <Link to={`/board?=boardId=${el.id}`} key={idx}>
+                      <img src="images/folder.png" />
+                      <div>{stringLimit(el.name, 7)}</div>
+                    </Link>
+                  </BoardIcon>
+                ) : (
+                  <BoardIcon onClick={openLoginModalHandler}>
+                    <img src="images/folder.png" />
+                    <div>{stringLimit(el.name, 7)}</div>
+                  </BoardIcon>
+                ))
+              );
+            })}
+          </BoardIconWrap>
+          {roleType === 'ADMIN' && (
+            <AdminPageButton onClick={moveToAdmin}>
+              <img src="images/adminIcon.png" alt="adminIcon" />
+              <div>Settings</div>
+            </AdminPageButton>
+          )}
+          ∂
+        </PageContainer>
+        <Header />
+      </AppContainer>
+    </>
   );
 }
--- client/src/pages/Main/styled.ts
+++ client/src/pages/Main/styled.ts
@@ -4,10 +4,12 @@ export const PageContainer = styled.div``;
 
 export const LogoImg = styled.div`
   position: absolute;
+  z-index: 0;
   top: 50%;
   left: 50%;
   transform: translate(-50%, -50%);
   img {
+    opacity: 0.4;
     height: 5rem;
   }
   ${({ theme }) => theme.media.tablet`
@@ -18,6 +20,71 @@ export const LogoImg = styled.div`
   ${({ theme }) => theme.media.desktop`
 		img {
 			height: 6rem;
+		}
+	`}
+`;
+
+export const BoardIconWrap = styled.div`
+  position: absolute;
+  z-index: 1;
+  top: 2rem;
+  left: 2rem;
+  display: grid;
+  grid-template-rows: repeat(3, auto);
+  grid-auto-flow: column;
+  row-gap: 1rem;
+  column-gap: 1rem;
+  place-items: center center;
+  plage-content: start start;
+  ${({ theme }) => theme.media.tablet`
+		top: 1.5rem;
+		left: 1rem;
+		grid-template-rows: repeat(6, auto);
+	`}
+  ${({ theme }) => theme.media.desktop`
+		top: 1.5rem;
+		left: 1rem;
+		grid-template-rows: repeat(6, auto);
+	`}
+`;
+
+export const BoardIcon = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 8rem;
+  a {
+    display: flex;
+    flex-direction: column;
+  }
+  img {
+    object-fit: contain;
+    height: 5.5rem;
+  }
+  div {
+    overflow-y: hidden;
+    text-align: center;
+    font-weight: 600;
+    font-size: 1.3rem;
+    color: #fff;
+    margin: 0.3rem 0;
+    padding: 0 3px;
+  }
+  ${({ theme }) => theme.media.tablet`
+		width: 7rem;
+		img {
+			height: 4rem;
+		}
+		div {
+			font-size: 1rem;
+		}
+	`}
+  ${({ theme }) => theme.media.desktop`
+		width: 7rem;
+		img {
+			height: 4rem;
+		}
+		div {
+			font-size: 1rem;
 		}
 	`}
 `;
@@ -38,7 +105,6 @@ export const AdminPageButton = styled.div`
     font-weight: 600;
     font-size: 1.3rem;
     color: #fff;
-    background: #001fe7;
     margin: 0.3rem 0;
     padding: 0 3px;
   }
